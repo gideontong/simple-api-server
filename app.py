e7a838af899e05b9b80d4441ba1d46f5fe0da614
Gideon Tong <gideon@gideontong.com>
2020-07-23 15:34:04 -0700

    Load some JSON

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from json import loads
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET', 'POST'])
-def get_api():
-    if request.method == 'POST':
-        print(request.form)
-        return '{"challenge":"35902439"}'
+@app.route('/')
+def route_home():
+    return "Hello, world!"
+
+@app.route('/api', methods=['POST'])
+def route_api():
+    data = {}
+    try:
+        data = loads(request.data)
+    except:
+        print("Didn't get a JSON input!")
+    return '{"challenge":"35902439"}'
 
 if __name__ == '__main__':
     app.run(port=80)
